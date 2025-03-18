313157e39333015f1abe4b353e49b5464d4810ea
arajun <arajyn0904@gmail.com>
2025-03-18 11:32:42 +0900

    lspによる自動補完、ホバー、フォーマット

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.o.tabstop = 4          -- タブ幅を4に設定
 vim.o.shiftwidth = 4       -- 自動インデントの幅を4に設定
 vim.o.expandtab = true     -- タブをスペースに変換
 vim.o.smartindent = true   -- スマートインデントを有効化
+vim.o.updatetime = 1000    -- ホバー発火までの時間
 --カーソル目立たせ
 vim.o.cursorline = true
 vim.o.cursorcolumn = true
@@ -24,16 +25,44 @@ vim.g.netrw_keepdir = 0
 vim.keymap.set('i', '<Tab>', '<C-n>')
 vim.keymap.set('i', '<S-Tab>', '<C-p>')
 
-vim.api.nvim_create_autocmd( 'InsertCharPre', {
-    callback = function()
-        if vim.fn.pumvisible() ==1 then
-            return
-        end
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "i", true)
-    end
-})
 
 vim.api.nvim_create_autocmd( 'FileType', {
     pattern = "html",
-    callback = function() vim.keymap.set('i', '</', '</<C-x><C-o><C-n><ESC>F>a') end,
+    callback = function() 
+        --vim.keymap.set('i', '</', '</<C-x><C-o><C-n><ESC>F>a') 
+        vim.lsp.start({
+            name = "html",
+            cmd = { "vscode-html-language-server", "--stdio" },
+            root_dir = vim.loop.cwd(),
+            on_attach = function(client, bufnr)
+                vim.api.nvim_create_autocmd( 'InsertLeave', {
+                    callback = function()
+                        vim.lsp.codelens.refresh({ bufnr = 0 })
+                        vim.lsp.buf.format()
+                   end
+                })
+                vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, { --ホバー表示
+                    callback = function() 
+                        vim.lsp.buf.hover() 
+                    end,
+                })
+                vim.api.nvim_create_autocmd( 'InsertCharPre', { --自動補完
+                    callback = function()
+                        if vim.fn.pumvisible() ==1 then
+                            return
+                        end
+                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-x><C-o>", true, true, true), "i", true)
+                    end
+                })
+            end,
+            init_options = {
+                configurationSection = { "html", "css", "javascript" },
+                embeddedLanguages = {
+                    css = true,
+                    javascript = true
+                },
+                provideFormatter = true
+            }
+        })
+    end,
 })
