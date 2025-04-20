7b8afad1755656c76e2e18d42b82dbf000d2b6a1
arajun <arajun>
2025-04-20 09:44:17 +0900

    commit

--- link.sh
+++ link.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-for file in $(ls -f | grep "^\.\w"); do
-    ln -fs ${PWD}/${file} ${HOME}
+echo ".configディレクトリ以外をリンク"
+
+for file in $(find . | sed -e "/.git/d" -e "/.config/d" -e "s/^.\///g" | grep "^\.\w"); do
+    ln -bs ${PWD}/${file} ${HOME}
 done
+
+echo "完了"
+
+echo ".configディレクトリをリンク"
+
+for file in $(ls .config); do
+
+    #ディレクトリが既に存在する場合退避
+    for testfile in $(ls ${HOME}/.config); do
+        if [ ${testfile} == ${file} ]; then
+            mv ${HOME}/.config/${file} ${HOME}/.config/${file}_old
+        fi
+    done
+
+    ln -fs ${PWD}/.config/${file} ${HOME}/.config
+done
+
+echo "完了"
